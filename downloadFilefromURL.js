45b1c27bf4079cf55c7e834208da3727c09a4a57
himanshushub <himanshushub8@gmail.com>
2021-06-05 19:45:05 +0530

    - download file from url

---
+++ downloadFilefromURL.js
@@ -0,0 +1,24 @@
+//  link or the code https://www.codegrepper.com/code-examples/javascript/node.js+download+file+from+url
+
+const https = require("https");
+const fs = require("fs");
+
+// url will have https url. If you want to use http url just replace https with http everywhere
+// dest will have filepath in whihc you wanna write the file e.g --> ./src/assets/img.png
+// cb is callback funtion
+
+let download = function (url, dest, cb) {
+  let file = fs.createWriteStream(dest);
+  let request = https
+    .get(url, function (response) {
+      response.pipe(file);
+      file.on("finish", function () {
+        file.close(cb); // close() is async, call cb after close completes.
+      });
+    })
+    .on("error", function (err) {
+      // Handle errors
+      fs.unlink(dest); // Delete the file async. (But we don't check the result)
+      if (cb) cb(err.message);
+    });
+};
